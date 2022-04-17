4807fe3d1f3ab00f57cf63e3c47e1354ac8f895d
Dyachenko Stanislav <boar_100@mail.ru>
2022-04-17 14:59:38 +0500

    17.04.2022

--- app/src/main/java/com/example/customview16042022/ImageEditTextButtonLayout.kt
+++ app/src/main/java/com/example/customview16042022/ImageEditTextButtonLayout.kt
@@ -13,8 +13,6 @@ class ImageEditTextButtonLayout(context: Context, attributeSet: AttributeSet) :
 
         val imageView = findViewById<ImageView>(R.id.image)
         val textView = findViewById<TextView>(R.id.text)
-        val editText = findViewById<EditText>(R.id.edit_query)
-        val editText2 = findViewById<EditText>(R.id.edit_query2)
 
 
         val set =
@@ -22,14 +20,10 @@ class ImageEditTextButtonLayout(context: Context, attributeSet: AttributeSet) :
 
         val image = set.getDrawable(R.styleable.ImageEditTextButtonLayout_image)
         val text = set.getString(R.styleable.ImageEditTextButtonLayout_text)
-        val edit = set.getString(R.styleable.ImageEditTextButtonLayout_edit)
-        val edit2 = set.getString(R.styleable.ImageEditTextButtonLayout_edit2)
 
 
         imageView.setImageDrawable(image)
         textView.text = text
-        editText.setText(edit)
-        editText2.setText(edit2)
 
 
     }
